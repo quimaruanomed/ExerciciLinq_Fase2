f7c070668c1d6f53f44b5961c5c7a7a94365d112
quimaruanomed <jruanomed@gmail.com>
2020-12-07 07:11:06 +0100

    first commit

---
+++ ExerciciLinq_FaseII/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+
+namespace ExerciciLinq_FaseII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numFaseI = {2, 6, 8, 4, 5, 5, 9, 2, 1, 8, 7, 5, 9, 6, 4};
+           
+            double average = numFaseI.Average(); //Calcula la Media 
+            int max = numFaseI.Max(); //Calcula el Maximo
+            int min = numFaseI.Min(); // Calcula el Minimo 
+
+           
+            Console.WriteLine($"Nota media : {average}  Nota maxima: {max}  Nota Minima: {min}");
+           
+
+        }
+    }
+}
